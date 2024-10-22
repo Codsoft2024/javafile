bef2cfc5a2cd4effa104fb4ffa0af70719bfe8fd
Codsoft2024 <saisiddharthakeesara@gmail.com>
2024-10-22 12:15:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
 	public static void main(String [] args) {
   		System.out.println("from remote computer");
-		System.out.println("from remote computer");
+		System.out.print("from remote computer");
   }
 }
 
